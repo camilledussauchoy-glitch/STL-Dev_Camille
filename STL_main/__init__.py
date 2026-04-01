c65135bb1e80239fc86f5dc3aad5ac94b1899308
Tibi Daniel <daniel.tibi@etu.ec-lyon.fr>
2026-04-01 12:35:21 +0200

    Add STL package installation warning

--- STL_main/__init__.py
+++ STL_main/__init__.py
@@ -1,3 +1,12 @@
-from importlib.metadata import version
+import warnings
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = version("STL")
+try:
+    __version__ = version("STL")
+except PackageNotFoundError:
+    __version__ = None
+
+    warnings.warn(
+        "In development mode: Please run 'pip install -e .' in the root directory of the STL repository to properly install the package, its dependencies and enable proper version tracking.",
+        UserWarning,
+    )
